08d212ad2be2162bfbb4515ba74b3864556269fb
Schlumpfi <272270803+Schlumpfraeuber@users.noreply.github.com>
2026-04-01 17:36:18 +0200

    adding ui folder and toggle menu script with animation

--- Assets/Scripts/UI/OpenMenuAnimation.cs
+++ Assets/Scripts/UI/OpenMenuAnimation.cs
@@ -1,28 +1,45 @@
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
+using System.Collections;
 using UnityEngine.InputSystem;
 
 public class OpenMenuAnimation : MonoBehaviour
 {
     [Header("animations")]
-    public RectTransform panel;      // Dein Panel hier im Inspector ziehen
+    public RectTransform panel;      
     public float duration = 1f;   
-
     private Vector2 startPosition;
     private Vector2 targetPosition;
     private float time;
+
     [Header("input")]
 
-    public UIDocument input;
-    
+    private UiInputs input;
+    private bool isOpen = false;
+    private bool isMoving = false;
+
+    void Awake()
+    {
+        input = new UiInputs();
+
+        input.UI.ToggleMenu.performed += ctx => ToggleMenu();
+    }
+
+    void OnEnable()
+    {
+        input.Enable();
+    }
+
+    void OnDisable()
+    {
+        input.Disable();
+    }
+
     void Start()
     {
         
         RectTransform parentRect = panel.parent.GetComponent<RectTransform>();
-        targetPosition = Vector2.zero; // AnchoredPosition (0,0) = Mitte bei mittigen Anchors
+        targetPosition = Vector2.zero;
 
-        
         startPosition = new Vector2(0, -parentRect.rect.height / 2f - panel.rect.height / 2f);
         panel.anchoredPosition = startPosition;
     }
@@ -37,5 +54,38 @@ public class OpenMenuAnimation : MonoBehaviour
             float t = Mathf.SmoothStep(0, 1, time / duration); // Smooth Bewegung
             panel.anchoredPosition = Vector2.Lerp(startPosition, targetPosition, t);
         }
+    }
+
+    void ToggleMenu()
+    {
+         if (isMoving) return;
+
+        isOpen = !isOpen;
+
+        Vector2 target = isOpen ? targetPosition : startPosition;
+
+        StartCoroutine(AnimatePanel(target));
+
+    }
+
+    IEnumerator AnimatePanel(Vector2 target)
+    {
+        isMoving = true;
+
+        Vector2 current = panel.anchoredPosition;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float t = Mathf.SmoothStep(0, 1, time / duration);
+
+            panel.anchoredPosition = Vector2.Lerp(current, target, t);
+
+            yield return null;
+        }
+
+        panel.anchoredPosition = target;
+        isMoving = false;
     }
 }
