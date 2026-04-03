caeae460249bad36855db39cec9ee6f92b70fded
Schlumpfi <272270803+Schlumpfraeuber@users.noreply.github.com>
2026-04-03 23:46:10 +0200

    adding functions to button

--- Assets/Scripts/UI/mainmenu/buttonstart.cs
+++ Assets/Scripts/UI/mainmenu/buttonstart.cs
@@ -2,5 +2,42 @@ using UnityEngine;
 
 public class buttonstart : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject tutorialpanle;
+    [SerializeField] private GameObject settingspanle;
+    [SerializeField] private GameObject quitpanle;
+
+    private void CloseMenuePanles ()
+    {
+        quitpanle.SetActive(false);
+        tutorialpanle.SetActive(false);
+        settingspanle.SetActive(false);
+    }
+
+    void Start()
+    {
+        CloseMenuePanles();
+    }
+
+    public void Quitbtn()
+    {
+        CloseMenuePanles();
+        quitpanle.SetActive(true);
+    }
+
+    public void Tutorialbtn()
+    {
+        CloseMenuePanles();
+        tutorialpanle.SetActive(true);
+    }
+
+    public void Playbtn()
+    {
+        CloseMenuePanles();
+    }
+
+    public void Settingbtn()
+    {
+        CloseMenuePanles();
+        settingspanle.SetActive(true);
+    }
 }
