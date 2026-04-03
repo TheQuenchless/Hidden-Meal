4b5551bd04bced64253c9802587c033f4eedf580
Schlumpfi <272270803+Schlumpfraeuber@users.noreply.github.com>
2026-04-03 13:08:32 +0200

    Merge branch 'main' of https://github.com/TheQuenchless/Hidden-Meal

---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public PoliceAI police;
+    [SerializeField]private TMP_Text text;
+    private float startshift;
+    private float shifttimer;
+    private float  timeleft;
+    private int remainingSeconds;
+
+
+
+    void Start()
+    {
+        startshift = police.shiftStartTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       getValue();
+       calculateCountdown();       
+    }
+
+    void getValue()
+    {
+        shifttimer = police.shiftTimer;
+    }
+
+    void calculateCountdown()
+    {
+        timeleft = Mathf.Max(0,startshift - shifttimer);
+        remainingSeconds = Mathf.CeilToInt(timeleft);
+        
+        text.text = remainingSeconds.ToString();
+    }
+}
